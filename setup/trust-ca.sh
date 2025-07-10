85ddb7cb477a64bb7aebd5b5912ace304a03a45c
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-07-11 01:06:49 +0200

    chore: rotate certificate

--- setup/trust-ca.sh
+++ setup/trust-ca.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Trusting self signed root CA certificate..."
-sudo cp ../apps/cert-manager/clusterissuer/cert/tls.crt /usr/local/share/ca-certificates/my-ca.crt 
-sudo cp ../apps/cert-manager/clusterissuer/cert/tls.crt /etc/pki/ca-trust/source/anchors/my-ca.crt
+sudo cp ../apps/cert-manager/clusterissuer/tls.crt /usr/local/share/ca-certificates/home-cluster.crt 
+sudo cp ../apps/cert-manager/clusterissuer/tls.crt /etc/pki/ca-trust/source/anchors/home-cluster.crt 
 sudo update-ca-certificates
 sudo update-ca-trust
